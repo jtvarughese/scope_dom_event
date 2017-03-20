4587bca5e0bffe6bc3b8b902f19f57c02c71bd5a
Joel Varughese <jtvarughese@gmail.com>
2017-03-20 17:42:06 -0400

    scope dom event html and js

---
+++ logic.js
@@ -0,0 +1,17 @@
+// if (5<10) {
+//   console.log("5 is less than 10")
+// } else {
+//   console.log("5 is not less than 10")
+// }
+//
+// var number = 3242
+// if (number<10) {
+//   alert("this is less than 10")
+// } else {
+//   alert("this is not less than 10")
+// }
+
+var string = "cheese"
+var string2 = "whiz"
+
+if 
